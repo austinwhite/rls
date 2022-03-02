5ebd3b2bdb7c741c13910aa424f47ad38fc881bf
Austin White <austinlldb@gmail.com>
2022-03-01 22:13:40 -0700

    basic ls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use std::path::Path;
+use std::error::Error;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+	if let Err(ref e) = run(Path::new(".")) {
+		println!("{}", e);
+		process::exit(1);
+	}
+}
+
+fn run(dir: &Path) -> Result<(), Box<dyn Error>> {
+	if dir.is_dir() {
+		for entry in fs::read_dir(dir)? {
+				let entry = entry?;
+				let file_name = entry
+						.file_name()
+						.into_string()
+						.or_else(|f| Err(format!("Invalid entry: {:?}", f)))?;
+				println!("{}", file_name);
+		}
+	}
+	Ok(())
 }
