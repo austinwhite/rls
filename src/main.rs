38a735421e1a2093fdd0236ad6cd5b6aa4b5c081
Austin White <austinlldb@gmail.com>
2022-03-02 17:57:12 -0700

    long list implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate structopt;
 
 use std::fs;
-use std::path::Path;
 use std::error::Error;
 use std::process;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use chrono::{DateTime, Local};
 
 #[derive(StructOpt, Debug)]
 struct Opt {
@@ -22,15 +22,22 @@ fn main() {
 	}
 }
 
-fn run(dir: &Path) -> Result<(), Box<dyn Error>> {
+fn run(dir: &PathBuf) -> Result<(), Box<dyn Error>> {
 	if dir.is_dir() {
 		for entry in fs::read_dir(dir)? {
-				let entry = entry?;
-				let file_name = entry
-						.file_name()
-						.into_string()
-						.or_else(|f| Err(format!("Invalid entry: {:?}", f)))?;
-				println!("{}", file_name);
+			let entry = entry?;
+			let file_name = entry.path();
+
+			let metadata = entry.metadata()?;
+			let size = metadata.len();
+			let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
+
+			println!(
+				"{:>5} {} {:?}",
+				size,
+				modified.format("%_d %b %H:%M").to_string(),
+				file_name
+			);
 		}
 	}
 	Ok(())
